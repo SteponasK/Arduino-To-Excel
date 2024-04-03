0974862b55674dd941ebcbe696d491b8ba688438
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-04 01:07:47 +0300

    Added print to csv file functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import serial
 from serial.tools import list_ports
+import csv
 
 ports = list_ports.comports()
 print("PORTS: ", end="")
@@ -13,11 +14,14 @@ if not ports:
 
 port_name = ports[0].device
 
-arduino = serial.Serial(port_name, 9600, 1)
+arduino = serial.Serial(port=port_name, baudrate=9600, timeout=1)
 
 first_line = arduino.readline().decode().strip()
 
-newLines = []
-while True:
-    newLines.append(arduino.readline().decode().strip())
-    
+csv_file_name = 'kiausiniu_info.csv'
+with open(csv_file_name, 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerows(first_line)
+    while True:
+        newLine = arduino.readline().decode().strip()
+        writer.writerow([newLine])
