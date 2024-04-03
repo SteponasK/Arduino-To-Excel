4ae01607a95069f44356e3c7d81fabd96bbb3a5e
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-04 00:53:12 +0300

    Print ports & choose the first one

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import serial
 from serial.tools import list_ports
+
+
+ports = list_ports.comports()
+print("PORTS: ", end="")
+for port in ports:
+    print(port.device, end=", ")
+print()
+
+if not ports:
+    print("No available ports.")
+    exit()
+
+port_name = ports[0].device
