50da98984e0593f41575355dfc874352a45e9da8
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-06 17:45:23 +0300

    Removed repeating code for csv file name

--- main.py
+++ main.py
@@ -56,19 +56,20 @@ class SerialThread(QThread):
         self.paused = False
 
 class CSVManager:
+    csv_file_name = 'kiausiniu_info.csv'
+
     @staticmethod
     def write_header():
         # Write title: "time, speed" to the csv file
         csv_file_name = 'kiausiniu_info.csv'
-        with open(csv_file_name, 'w', newline='') as csv_file:
+        with open(CSVManager.csv_file_name, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(["time", "speed"])
 
     @staticmethod
     def append_data(time, speed):
         # Append further data to the csv file
-        csv_file_name = 'kiausiniu_info.csv'
-        with open(csv_file_name, 'a', newline='') as csv_file:
+        with open(CSVManager.csv_file_name, 'a', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow([time, speed])
 
