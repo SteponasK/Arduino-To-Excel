34591cf5b1d3a18698b53704caecf70338d9b75e
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-06 20:13:03 +0300

    Added csv_to_excel_convertor class

---
+++ Arduino_To_Excel/csv_to_excel_convertor.py
@@ -0,0 +1,19 @@
+import csv
+from openpyxl import Workbook
+
+class CsvToExcelConvertor():
+    @staticmethod
+    def csv_to_excel(csv_file, excel_file):
+        # Creating workbook & selecting the stylesheet
+        wb = Workbook()
+        ws = wb.active
+
+        with open(csv_file, 'r') as file:
+            csv_reader = csv.reader(file)
+            
+            # Writing values in the workbook
+            for row_index, row in enumerate(csv_reader, start=1):
+                for column_index, value in enumerate(row, start=1):
+                    ws.cell(row=row_index, column=column_index).value = value
+                    
+        wb.save(excel_file)
