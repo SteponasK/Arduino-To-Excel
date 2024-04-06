c0866e171be7dbd4964a1dcd7c4e9efb8e922665
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-04-06 20:17:34 +0300

    Separated file format string (.csv) from csv_filename

--- Arduino_To_Excel/main.py
+++ Arduino_To_Excel/main.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from main_window import MainWindow
-
+from csv_to_excel_converter import CsvToExcelConverter
+from csv_manager import CSVManager
 if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+    # CsvToExcelConverter.csv_to_excel(CSVManager.csv_file_name,)
